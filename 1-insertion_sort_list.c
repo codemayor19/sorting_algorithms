079426574e450424e7f8d96976e3f66e474f9cbc
codemayor19 <codemayor19@gmail.com>
2024-01-26 10:06:15 -0800

    new commit

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -23,7 +23,7 @@ void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
 
 /**
  * insertion_sort_list - Sorts a doubly linked list of integers
- * 			using the insertion sort algorithm.
+ * using the insertion sort algorithm.
  * @list: A pointer to the head of a doubly-linked list of integers.
  *
  * Description: Prints the list after each swap.
